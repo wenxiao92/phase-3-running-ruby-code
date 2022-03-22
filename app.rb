c36c99d02d3bcd12582ae1d463fe47d305f4672a
Wen Xiao <wennstyle@gmail.com>
2022-03-21 23:34:46 -0400

    commit tree 978adb8a9eb83c0a3866f297f94e1b277f10fba2

---
+++ app.rb
@@ -0,0 +1,8 @@
+#puts [1, 2, 3].inspect
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+#puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
